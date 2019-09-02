903e62f390eca5645ff62d469b82c902705b3437
“Sam <sam.alsmadi@gmail.com>
2019-09-02 03:05:19 -0400

    chore: add link to palette

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -1,4 +1,3 @@
-
 import React from "react";
 import { withStyles } from "@material-ui/styles";
 
@@ -56,7 +55,7 @@ function MiniPalette(props) {
   ));
 
   return (
-    <div className={classes.root}>
+    <div className={classes.root} onClick={props.handleClick}>
       <div className={classes.colors}>{miniColorBoxes}</div>
       <h5 className={classes.title}>
         {paletteName} <span className={classes.emoji}>{emoji}</span>
