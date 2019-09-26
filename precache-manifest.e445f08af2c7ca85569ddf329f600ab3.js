65ff9ba8d95802c97d00f971dae341ff975121bd
“Sam <sam.alsmadi@gmail.com>
2019-09-25 23:06:56 -0400

    Updates

---
+++ precache-manifest.e445f08af2c7ca85569ddf329f600ab3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c9d7d7d9fe9c1e1c591a361c0d1a3ce",
+    "url": "/ui-color-picker/index.html"
+  },
+  {
+    "revision": "58eeb68332ff6059241b",
+    "url": "/ui-color-picker/static/css/2.e337d257.chunk.css"
+  },
+  {
+    "revision": "78b8e8ad4ec986fc1781",
+    "url": "/ui-color-picker/static/css/main.f4347978.chunk.css"
+  },
+  {
+    "revision": "58eeb68332ff6059241b",
+    "url": "/ui-color-picker/static/js/2.962857ee.chunk.js"
+  },
+  {
+    "revision": "78b8e8ad4ec986fc1781",
+    "url": "/ui-color-picker/static/js/main.6a28b329.chunk.js"
+  },
+  {
+    "revision": "8f946850518a9b5f2869",
+    "url": "/ui-color-picker/static/js/runtime~main.0a089539.js"
+  },
+  {
+    "revision": "67decdcf761f5faed0f6b1fa6a5d2b51",
+    "url": "/ui-color-picker/static/media/bg.67decdcf.svg"
+  }
+]);
